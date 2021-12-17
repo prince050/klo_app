9949fdf36fdf39402dd701a8292146a247b3d9ab
prince050 <nyamsiprince@gmail.com>
2021-12-17 17:09:55 +0100

    update

---
+++ src/test/java/com/klo/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.klo.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
